d975c2e667ee07fae8051fe6d90126ff230f8e4c
Nidhish-Agarwal <nidhishagarwal14@gmail.com>
2026-01-28 11:58:53 +0530

    Setup postgress sql and redis

---
+++ backend/fastapi_app/app/core/config.py
@@ -0,0 +1,11 @@
+# app/core/config.py
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    REDIS_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
